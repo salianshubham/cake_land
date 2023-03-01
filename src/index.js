d810651d2f19ad6b6afc09bcb4b69e15212b68eb
Shubham <shubhamsalian46@gmail.com>
2023-03-01 19:55:07 +0530

    day3

--- src/index.js
+++ src/index.js
@@ -4,10 +4,34 @@ import "./index.css";
 
 ReactDOM.render(
   <>
-    <div className="divname">
-        {/* <h1>shubham</h1> */}
-        
-    </div>
+    <section>
+      <div className="divname">
+        <header>
+          <h2 class="logo">CAKE LAND</h2>
+          <nav class="navigation">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+          </nav>
+        </header>
+        <footer>
+          <h1>dnsjkd</h1>
+        </footer>
+        <div></div>
+        {/* <img src='./images/front_page1.jpg' alt="Italian Trulli"></img> */}
+      </div>
+    </section>
+    <footer>
+      <h1>sjncmncmn</h1>
+      <h1>sjncmncmn</h1>
+      <h1>sjncmncmn</h1>
+      <h1>sjncmncmn</h1>
+      <h1>sjncmncmn</h1>
+      <h1>sjncmncmn</h1>
+      <h1>sjncmncmn</h1>
+      <h1>sjncmncmn</h1>
+    </footer>
   </>,
   document.getElementById("root")
 );
