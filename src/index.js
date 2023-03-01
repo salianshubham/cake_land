bd55560b842b4e26d7d0055d16e162f904e0814f
Shubham <shubhamsalian46@gmail.com>
2023-03-01 20:08:02 +0530

    day_3

--- src/index.js
+++ src/index.js
@@ -5,33 +5,31 @@ import "./index.css";
 ReactDOM.render(
   <>
     <section>
-      <div className="divname">
-        <header>
-          <h2 class="logo">CAKE LAND</h2>
-          <nav class="navigation">
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Contact</a>
-          </nav>
-        </header>
-        <footer>
-          <h1>dnsjkd</h1>
-        </footer>
-        <div></div>
-        {/* <img src='./images/front_page1.jpg' alt="Italian Trulli"></img> */}
+      <div className="front_Page">
+        <div className="front_Page container">
+          <header>
+            <h2 class="logo">CAKE LAND</h2>
+            <nav class="navigation">
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#">Services</a>
+              <a href="#">Contact</a>
+            </nav>
+          </header>
+        </div>
       </div>
     </section>
-    <footer>
-      <h1>sjncmncmn</h1>
-      <h1>sjncmncmn</h1>
-      <h1>sjncmncmn</h1>
-      <h1>sjncmncmn</h1>
-      <h1>sjncmncmn</h1>
-      <h1>sjncmncmn</h1>
-      <h1>sjncmncmn</h1>
-      <h1>sjncmncmn</h1>
-    </footer>
+
+    {/* middle page */}
+
+    <section id="services">
+      <div className="middlePage middlePageContainer">
+        {/* <div className="div1">BOX1</div>
+        <div className="div2">BOX2</div>
+        <div className="div3">BOX3</div>
+        <div className="div4">BOX4</div> */}
+      </div>
+    </section>
   </>,
   document.getElementById("root")
 );
